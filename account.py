ef4b6c957778b707fd8bbe2dca717a7e31c5b953
voolyvex <1joeknapp@gmail.com>
2022-11-16 17:00:50 -0600

    in account.py create Account class and __init__ method + create deposit, withdraw, get_balance, and get_name methods

--- account.py
+++ account.py
@@ -1,1 +1,22 @@
 
+
+class Account:
+    def __init__(self, name: str) -> None:
+        self.__account_name = name
+        self.__account_balance = 0
+    
+    def deposit(self, amount: float) -> bool:
+        if amount <= 0:
+            return False
+        else:
+            self.__account_balance += amount
+            return True
+    
+    def withdraw(self, amount: float) -> bool:
+        if amount <= 0 or amount > self.__account_balance:
+            return False
+        else:
+            self.__account_balance -= amount
+            return True
+
+
