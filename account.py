633cb7f3f0d1c07d69e4ac5940cd826ed5039bb7
voolyvex <1joeknapp@gmail.com>
2022-11-17 09:58:11 -0600

    add type hinting to account.py

--- account.py
+++ account.py
@@ -1,1 +1,28 @@
 
+
+class Account:
+    def __init__(self, name: str) -> None:
+        self.__account_name = name
+        self.__account_balance = 0
+    
+    def deposit(self, amount: float) -> bool:
+        if amount <= 0:
+            return False
+        else:
+            self.__account_balance += amount
+            return True
+    
+    def withdraw(self, amount: float) -> bool:
+        if amount <= 0 or amount > self.__account_balance:
+            return False
+        else:
+            self.__account_balance -= amount
+            return True
+    
+    def get_balance(self) -> float:
+        return self.__account_balance
+
+    def get_name(self) -> str:
+        return self.__account_name
+
+
