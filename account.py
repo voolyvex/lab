d8040c617e0074b18697766ab944312ca306ace2
voolyvex <1joeknapp@gmail.com>
2022-11-17 09:54:35 -0600

    restore original account.py (no typehinting) in main branch

--- account.py
+++ account.py
@@ -1,43 +1,28 @@
 
 
 class Account:
-<<<<<<< Updated upstream
-    def __init__(self, name: str) -> None:
-        self.__account_name = name
-        self.__account_balance = 0
-    
-    def deposit(self, amount: float) -> bool:
-=======
     def __init__(self, name):
         self.__account_name = name
         self.__account_balance = 0
     
     def deposit(self, amount):
->>>>>>> Stashed changes
         if amount <= 0:
             return False
         else:
             self.__account_balance += amount
             return True
     
-<<<<<<< Updated upstream
-    def withdraw(self, amount: float) -> bool:
-=======
     def withdraw(self, amount):
->>>>>>> Stashed changes
         if amount <= 0 or amount > self.__account_balance:
             return False
         else:
             self.__account_balance -= amount
             return True
-<<<<<<< Updated upstream
-=======
     
     def get_balance(self):
         return self.__account_balance
 
     def get_name(self):
         return self.__account_name
->>>>>>> Stashed changes
 
 
