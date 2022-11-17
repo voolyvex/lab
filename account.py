f4bb756aa4bf2b9cf753b279d7f42b8efe66bbcc
voolyvex <1joeknapp@gmail.com>
2022-11-17 14:39:21 -0600

    added Docstrings to account.py methods

--- account.py
+++ account.py
@@ -2,10 +2,19 @@
 
 class Account:
     def __init__(self, name: str) -> None:
+        """
+        Assigns an Account Name and Account Balance to an Account object when it is instantiated.
+        :param name: Account name.
+        """
         self.__account_name = name
-        self.__account_balance = 0
+        self.__account_balance: float = 0
     
     def deposit(self, amount: float) -> bool:
+        """
+        Method to add money to an account.
+        :param amount: The deposit amount.
+        :return: True if deposit is succesful, otherwise False.
+        """
         if amount <= 0:
             return False
         else:
@@ -13,6 +22,11 @@ class Account:
             return True
     
     def withdraw(self, amount: float) -> bool:
+        """
+        Method to deduct money from an account.
+        :param amount: The withdrawal amount.
+        :return: True if withdrawal is succesful, otherwise False.
+        """
         if amount <= 0 or amount > self.__account_balance:
             return False
         else:
@@ -20,9 +34,17 @@ class Account:
             return True
     
     def get_balance(self) -> float:
+        """
+        Method to access an account's balance.
+        :return: The account balance.
+        """
         return self.__account_balance
 
     def get_name(self) -> str:
+        """
+        Method to modify an account's balance.
+        :return: The account name.
+        """
         return self.__account_name
 
 
